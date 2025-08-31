b95caed7f70fd49a424208a81bff31db95d885d7
Andy Cao <taicao48@gmail.com>
2025-08-31 15:13:46 -0700

    Week 2

---
+++ index.js
@@ -0,0 +1,31 @@
+const myhttp = require("http");
+
+const fs = require('fs').promises;
+
+const requestListener = function( myrequest, myresponse) {
+
+        console. log( myrequest.url );
+        
+    if ( myrequest.url === "/"){
+        fs.readFile (__dirname + "/web.html").then(
+            contents => {
+            myresponse.setHeader("Content-Type", "text/html; charset=UTF-8");
+            myresponse.writeHead(200);
+            myresponse.end(contents);
+         });
+    } else {
+        fs.readFile (__dirname + "/links.json").then(
+            contents => {
+            myresponse.setHeader("Content-Type", "application/json; charset=UTF-8");
+            myresponse.writeHead(200);
+            myresponse.end(contents);
+         });
+    }
+
+    };
+    const myserver = myhttp.createServer( 
+
+        requestListener
+);
+
+myserver.listen( 8080, "127.0.0.1" );
